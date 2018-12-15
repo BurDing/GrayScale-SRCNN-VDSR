370ca6fafd5758fec2f41e1dffc71306244a8f0d
Cheng Ding <chengd2@illinois.edu>
2018-12-15 15:39:51 -0600

    a

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import imageio
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-epoch = 200
+epoch = 100
 cuda = True
 batch = 1
 train_size = 16000
@@ -80,7 +80,7 @@ for i in range(0, epoch):
         loss_input_sum += loss_input.item()
         loss.backward()
         optimizer.step()    # Does the update
-        if (step + 1) % 400 == 0:
+        if (step + 1) % 4000 == 0:
             print("epoch: " + str(i) + " step: " + str(step) + " loss: " + str(loss_sum) + " loss_input: " + str(loss_input_sum))
 
 torch.save(net, "train_model.pth")
