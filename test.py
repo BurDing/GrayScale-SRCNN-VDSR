f3c1ad6d32d8642043204fd8bc4a5ffd88974a8f
Cheng Ding <chengd2@illinois.edu>
2018-12-15 20:42:11 -0600

    a

--- test.py
+++ test.py
@@ -23,6 +23,24 @@ for i in range(0, test_size):
 test_data = [torch.FloatTensor(i).view(1, 128, 128) for i in test_imgs]
 test_loader = DataLoader(dataset=test_data, batch_size=batch, shuffle=True)
 
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN,self).__init__()
+        self.conv1 = nn.Conv2d(1,64,kernel_size=9,padding=4);
+        self.relu1 = nn.ReLU();
+        self.conv2 = nn.Conv2d(64,32,kernel_size=1,padding=0);
+        self.relu2 = nn.ReLU();
+        self.conv3 = nn.Conv2d(32,1,kernel_size=5,padding=2);
+
+    def forward(self,x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.conv3(out)
+
+        return out
+
 model = torch.load("model/500_train_model.pth")
 
 # test
