ff4ce4b94a68dc8f9b2798f5d4da28668b54ce09
Cheng Ding <chengd2@illinois.edu>
2018-12-15 20:38:01 -0600

    a

--- test.py
+++ test.py
@@ -26,12 +26,11 @@ test_loader = DataLoader(dataset=test_data, batch_size=batch, shuffle=True)
 model = torch.load("model/500_train_model.pth")
 
 # test
-i = 0
 for step, input in enumerate(test_loader):
+    print(step)
     if cuda:
         model = model.cuda()
         input = input.cuda()
     out = model(input).view(batch,128,128).detach().numpy()
     for j in range(0, batch):
-        imageio.imwrite('upload/' + test_files[i * batch + j], out[j], format=None)
-    i += 1
+        imageio.imwrite('upload/' + test_files[step * batch + j], out[j], format=None)
